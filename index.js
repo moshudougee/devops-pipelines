5999b1b0d1a07b18f5bd85d3aa009436df466619
moshudougee <mbuthiadenis@gmail.com>
2025-09-13 03:08:47 +0300

    feat: Add CI Pipeline

---
+++ index.js
@@ -0,0 +1,2 @@
+console.log("This is the devops-pipelines index file.");
+console.log("I'm learning CI/CD pipelines! using GitHub Actions, and more.");
---
+++ test.js
@@ -0,0 +1,3 @@
+console.log("Tests starting...");
+setTimeout(() => console.log("Waiting 3 seconds..."), 3000);
+console.log("Tests completed.");
